17b6353676b07212b14c5d11fa30bf0261615dca
Jaxon Wang <jxwang92@gmail.com>
2015-05-04 17:49:19 +0800

    add test item sim test scipt

--- tinyrec/test/core/get_item_similarity_list.py
+++ tinyrec/test/core/get_item_similarity_list.py
@@ -7,7 +7,19 @@ config.Config().configdict['user_item_CF']['similarity'] = 'adjusted_cos'
 config.Config().apply_changes()
 dao = sim.new_DAO_interface()
 
-l = dao.get_item_sim_list(50,-1)
+maxitemid = config.Config().configdict['dataset']['maxitemid']
+maxuserid = config.Config().configdict['dataset']['maxuserid']
+sim.init_user_mean_matrix(dao)  
+
+ri = 50
+ril = dao.get_user_list_by_item(ri)
+l = []
+for i in range(1,maxitemid + 1):
+    il = dao.get_user_list_by_item(i)
+    if il == []:
+        continue
+    sima = sim.similarity_func(ril,il,maxuserid + 1)
+    l.append((i,sima))
 
 for i,s in l:
     print i,s
